5d84a9868be457d8541b02f3aeddd1993dbb8fa6
Shaun <discospider@me.com>
2013-07-20 22:32:53 -0400

    Added GPLv2 License

--- auto-gitlab-backup.sh
+++ auto-gitlab-backup.sh
@@ -10,6 +10,24 @@
 # another host
 ################################
 
+## GPL v2 License
+# auto-gitlab-backup
+# Copyright (C) 2013  Shaun Sundquist
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 ###
 ## Settings/Variables
 #
