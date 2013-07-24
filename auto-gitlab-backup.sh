e40b1fb2ce42219dae6428b56b2eb006965972b7
Shaun Sundquist <sund@unc.edu>
2013-07-24 12:56:51 -0400

    Issue #4: print out script tag and commit

--- auto-gitlab-backup.sh
+++ auto-gitlab-backup.sh
@@ -80,6 +80,12 @@ rsyncDaemon() {
     rsync -Cavz --port=$remotePort --password-file=$rsync_password_file --delete-after /$gitRakeBackups/ $remoteUser@$remoteServer::$remoteModule
 }
 
+printScriptver() {
+	# print the most recent tag
+	echo "This is $0"
+	echo "Version $(git describe --abbrev=0 --tags), commit #$(git log --pretty=format:'%h' -n 1)."
+}
+
 ###
 ## Git'r done
 #
@@ -118,6 +124,8 @@ else if [ -e $sshKeyPath -a -r $sshKeyPath ] && [[ $sshKeyPath != "" ]]
 	fi
 fi
 
+# Print version
+printScriptver
 
 ###
 ## Exit gracefully
