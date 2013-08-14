572cb33a63b5ba7c08bc4b1bf8d705bf987b1006
Shaun Sundquist <sund@unc.edu>
2013-08-14 14:57:24 -0400

    restore script works, well enough for now

--- restoreGitLab.sh
+++ restoreGitLab.sh
@@ -47,29 +47,53 @@ usage() {
     echo "USAGE:"
     echo "${0} -r [backup file]"
     echo "with only -r, I'll look for your gitlab install in the normal place"
-    echo "(/home/git/gitlab) or you an specify the full path to the backup"
+    echo "(/home/git/gitlab) "
     
     exit 0
 }
 
 sanityCheck() {
-	echo "git's home is determined to be : $gitHome"
+	echo "git's home is found to be : $gitHome"
 	echo "gitlab backups are found in : $gitlabBackups"
 }
 
 getFileList() {
-	echo "File list in : $1"
-	e=1
+	#echo "File list in : $1"
+	
+	# best not to use ls to get a listing of a dir
+	local e=1
 	shopt -s nullglob
 	for f in $1/*; do
 	    [[ -e $f ]] && [[ -f $f ]] || continue
-	    fileArray[$e]=$f
+	    fileArray[$e]=$(basename "$f")
 	    ((e++))
 	done
 	shopt -u nullglob
 	
-	## TO DO: sort array so newest is first
-
+	#echo "fileArray is : ${fileArray[@]}"
+	#echo "fileArray size is : ${#fileArray[@]}"
+	
+	## sort array so newest is first
+	
+	local c=1
+	for ((i=${#fileArray[@]}; i>=1; i--)); do
+		echo " sort i : $i & c $c"
+		REVfileArray[$c]="${fileArray[$i]}"
+		((c++))
+	done
+	
+	#echo "REVfileArray is : ${REVfileArray[@]}"
+	#echo "REVfileArray size is : ${#REVfileArray[@]}"
+		
+	## copy REVfileArray to fileArray
+	unset fileArray
+	
+	for ((i=${#REVfileArray[@]}; i>=1; i--)); do
+		fileArray[$i]="${REVfileArray[$i]}"
+	done
+	
+	#echo "did we reasign? : ${fileArray[@]}"
+	#echo "reset fileArray size is : ${#fileArray[@]}"
 }
 
 printFileList() {
@@ -83,6 +107,17 @@ printFileList() {
         echo "Element $e is : ${fileArray[$e]}"
         ((e++))
     done
+    
+    echo "Reversed is : ${REVfileArray[@]}"
+    
+	# print each elemt
+	local z=0
+    while [ $z -le ${#fileArray[@]} ]
+    do
+    	echo "Element $z of REVfileArray is : ${REVfileArray[$z]}"
+		((z++))
+	done
+
 
 }
 
@@ -102,7 +137,13 @@ verifyRestore() {
 
     *)
     	# if the file list is grather than 1, menu time
-        chooseBackup
+    	## if whiptale installed, use
+        if `command -v whiptail > /dev/null`
+        then
+        	chooseBackupWhiped
+        else
+        	chooseBackup
+        fi
         rakeRestore
 
     ;;
@@ -134,8 +175,8 @@ chooseBackup() {
 	echo "Choose from below which backup to restore from:"
 	echo "** The newest will be at the bottom of the list! **"
 	## TO DO: add option to exit in array at this point
+	## TO DO: invert array, newest on top
 	
-	Unix=("${Unix[@]}" "AIX" "HP-UX")
 	fileArray=("${fileArray[@]}" "Abort")
 	
 	select chosen in ${fileArray[@]};
@@ -153,39 +194,60 @@ chooseBackup() {
 }
 
 chooseBackupWhiped() {
-	echo "Choose from below which backup to restore from:"
-	
 	# make sure we don't have an empty list
 	WC=`echo ${fileArray[@]} | wc -l`
 	
 	if [[ "${WC}" -ne 0 ]]
 	then
-    	whiptail --backtitle "Welcome to restore" --title "Restore Files" \
-    --menu "Please select the file to restore" 14 40 6 "${fileArray[@]}"
+		# create the whippedList
+		i=0
+		s=1    # decimal ASCII "A" 
+		for f in ${fileArray[@]}
+			do
+				# convert to octal then ASCII character for selection tag
+				whippedListArray[i]="$f"
+				whippedListArray[i+1]=" "    # save file name
+				((i+=2))
+				((s++))
+		done
+		
+		## calc screen height and width
+		whipHeight=20 # find away to calc a pleasing height
+		whipWidth=42 # find away to calc a pleasing height
+		
+		# whiptail uses stdin and stdout to draw boxes, have to reassign with 3>&1 1>&2 2>&3
+    	chosen=$(whiptail --backtitle "Restore a GitLab Backup" --title "Restore from backup" \
+			--menu "Please select the file to restore" "$whipHeight" "$whipWidth" \
+			"${#fileArray[@]}" "${whippedListArray[@]}" 3>&1 1>&2 2>&3)
 	fi
 
-    # if [[ $? == 255 ]]
-# 	then
-# 	    do cancel stuff
-# 	fi
-
+	#echo " you chose : $chosen"
 	
 }
 
 rakeRestore() {
 	cd $gitlabDir
+	# if chosen is null then abort
+	if [ -z "$chosen" ]
+	then
+		echo "Cancelling restore..."
+		exit 1
+	fi
+	
 	backupfilename=$(basename "$chosen")
 	echo $backupfilename
 	timeStamp=${backupfilename%_gitlab_backup.tar}
 	echo "timestamp is : $timeStamp"
 	# restore the chosen backup
  	sudo -u git -H bundle exec rake gitlab:backup:restore RAILS_ENV=production BACKUP=$timeStamp
+ 	echo " rake gitlab:backup:restore returned : $?"
 }
 
 rakeRestoreSingle() {
 	cd $gitlabDir
 	# restore the only backup available; will complain if it finds more than one
  	sudo -u git -H bundle exec rake gitlab:backup:restore RAILS_ENV=production
+ 	echo " rake gitlab:backup:restore returned : $?"
 }
 
 permsFixBase() {
@@ -227,7 +289,6 @@ rakeCheck() {
  	sudo -u git -H bundle exec rake gitlab:check RAILS_ENV=production
 }
 
-
 ###
 ## Git'r done
 #
@@ -237,14 +298,17 @@ case "$1" in
         # find the backup and make a list
 		sanityCheck
         getFileList $gitlabBackups
-        printFileList
+        #printFileList
         verifyRestore
-        
+         #chooseBackupWhiped
         # fix things
-        #permsFixBase
+        ## if the rake fails (it will with different git home
+        # run the fixes
+        ## TO DO determine if we really need to run these
+        permsFixBase
         postRestoreLink
-        rakeCheck
         rakeInfo
+        rakeCheck
     ;;
     
     "-R")
@@ -264,7 +328,6 @@ case "$1" in
         postRestoreLink
         rakeInfo
         rakeCheck
-        
         
     ;;
 
